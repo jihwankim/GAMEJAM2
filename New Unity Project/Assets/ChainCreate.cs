45ce22a1b5990893a08dc8cd05cec39177dde1e3
Lee Sun-Hyoup <kciter@naver.com>
2014-05-24 21:04:07 +0900

    * 플레이어 고정, 툰 쉐이딩 적용

--- New Unity Project/Assets/ChainCreate.cs
+++ New Unity Project/Assets/ChainCreate.cs
@@ -9,15 +9,16 @@ public class ChainCreate : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		if (ChainCount > 0) {
-						Vector3 gap = this.renderer.bounds.size * 0.5f;
-						GameObject NextChain = (GameObject)GameObject.Instantiate (Chain, this.transform.position + Vector3.Scale (gap, this.transform.up), this.transform.rotation);// this.transform.up);
-						NextChain.transform.parent = this.transform;
-						NextChain.transform.localScale = new Vector3 (1.0f, 1.0f, 1.0f);
-						NextChain.GetComponent<ChainCreate> ().ChainCount = ChainCount - 1;
+			Vector3 gap = this.renderer.bounds.size * 0.5f;
+			GameObject NextChain = (GameObject)GameObject.Instantiate (Chain, this.transform.position + Vector3.Scale (gap, this.transform.up), this.transform.rotation);// this.transform.up);
+			NextChain.transform.parent = this.transform;
+			NextChain.transform.localScale = new Vector3 (1.0f, 1.0f, 1.0f);
+			NextChain.GetComponent<ChainCreate> ().ChainCount = ChainCount - 1;
 
-						this.GetComponent<HingeJoint> ().connectedBody = NextChain.GetComponent<Rigidbody> ();
-				} else
-						DestroyImmediate (this.GetComponent<HingeJoint> ());
+			this.GetComponent<HingeJoint> ().connectedBody = NextChain.GetComponent<Rigidbody> ();
+		} else {
+				DestroyImmediate (this.GetComponent<HingeJoint> ());
+		}
 			
 	}
 	
