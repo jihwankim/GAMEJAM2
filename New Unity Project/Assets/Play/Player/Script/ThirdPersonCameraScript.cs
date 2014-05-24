b14c35a7a52a45b36cae30c1eb596c813153440a
Lee Sun-Hyoup <kciter@naver.com>
2014-05-25 01:47:22 +0900

    * 플레이씬 변경

---
+++ New Unity Project/Assets/Play/Player/Script/ThirdPersonCameraScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCameraScript : MonoBehaviour {
+
+	public GameObject Player = null;
+	public float damping = 1f;
+	private Vector3 offset;
+
+	void Start () {
+		offset = Player.transform.position - new Vector3(2f, 1.5f, 0f);
+	}
+
+	void Update () {
+		float desiredAngle = Player.transform.eulerAngles.y+90f;
+		Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+
+		transform.position = Player.transform.position - (rotation * offset);
+
+//		float currentAngle = transform.eulerAngles.y;
+//		float desiredAngle = Player.transform.eulerAngles.y + 90f;
+//		float angle = Mathf.LerpAngle(currentAngle, desiredAngle, Time.deltaTime * damping);
+//
+//		Quaternion rotation = Quaternion.Euler(0, angle, 0);
+//		transform.position = Player.transform.position - (rotation * offset);
+
+		transform.LookAt(Player.transform);
+	}
+}
