299e178e21d1fa2122bf0286824cce376552a1e5
jihwankim <mastrayer@naver.com>
2014-05-24 20:39:31 +0900

    # ball bug fixed

--- New Unity Project/Assets/ChainCreate.cs
+++ New Unity Project/Assets/ChainCreate.cs
@@ -8,16 +8,17 @@ public class ChainCreate : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		if (ChainCount > 0) 
-		{
-			Vector3 gap =  this.renderer.bounds.size * 0.5f;
-			GameObject NextChain = (GameObject)GameObject.Instantiate (Chain, this.transform.position + Vector3.Scale(gap, this.transform.up), this.transform.rotation);// this.transform.up);
-			NextChain.transform.parent = this.transform;
-			NextChain.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-			NextChain.GetComponent<ChainCreate> ().ChainCount = ChainCount - 1;
+		if (ChainCount > 0) {
+						Vector3 gap = this.renderer.bounds.size * 0.5f;
+						GameObject NextChain = (GameObject)GameObject.Instantiate (Chain, this.transform.position + Vector3.Scale (gap, this.transform.up), this.transform.rotation);// this.transform.up);
+						NextChain.transform.parent = this.transform;
+						NextChain.transform.localScale = new Vector3 (1.0f, 1.0f, 1.0f);
+						NextChain.GetComponent<ChainCreate> ().ChainCount = ChainCount - 1;
 
-			this.GetComponent<HingeJoint> ().connectedBody = NextChain.GetComponent<Rigidbody>();
-		}
+						this.GetComponent<HingeJoint> ().connectedBody = NextChain.GetComponent<Rigidbody> ();
+				} else
+						DestroyImmediate (this.GetComponent<HingeJoint> ());
+			
 	}
 	
 	// Update is called once per frame
