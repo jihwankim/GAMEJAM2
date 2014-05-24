70600f4339565cddca0b1577e3966bbb794873cd
jihwankim <mastrayer@naver.com>
2014-05-24 19:47:21 +0900

    # chain bug fixed

--- New Unity Project/Assets/ChainCreate.cs
+++ New Unity Project/Assets/ChainCreate.cs
@@ -8,12 +8,12 @@ public class ChainCreate : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		//this.GetComponent<Rigidbody> ().Sleep ();// rigidbody = null;
 		if (ChainCount > 0) 
 		{
 			Vector3 gap =  this.renderer.bounds.size * 0.5f;
 			GameObject NextChain = (GameObject)GameObject.Instantiate (Chain, this.transform.position + Vector3.Scale(gap, this.transform.up), this.transform.rotation);// this.transform.up);
-
+			NextChain.transform.parent = this.transform;
+			NextChain.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
 			NextChain.GetComponent<ChainCreate> ().ChainCount = ChainCount - 1;
 
 			this.GetComponent<HingeJoint> ().connectedBody = NextChain.GetComponent<Rigidbody>();
