91d807659902450f9e8ce9ee390f5a3a8629efc8
Lee Sun-Hyoup <kciter@naver.com>
2014-05-25 01:32:43 +0900

    * 공격 속도 범위 수정

--- New Unity Project/Assets/PlayerAttackScript.cs
+++ New Unity Project/Assets/PlayerAttackScript.cs
@@ -4,12 +4,13 @@ using System.Collections;
 public class PlayerAttackScript : MonoBehaviour {
 
 	public float AttackSpeed = 1f;
+	public float AttackRange = 1f;
 
 	void Start () {
 	
 	}
 
 	void Update () {
-		GameObject.Find ("Weapon").transform.localPosition = new Vector3 (Mathf.Sin (Time.time*AttackSpeed) * Time.deltaTime * 10, 0f, 0f);
+		GameObject.Find ("Weapon").transform.localPosition = new Vector3 (Mathf.Sin (Time.time*AttackSpeed) * Time.deltaTime * AttackRange, 0f, 0f);
 	}
 }
